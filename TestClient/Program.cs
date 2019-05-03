a6cff3c1c8a536631b2d617a8568eacc44e3a3b9
kostia <kkomkov99@gmail.com>
2019-05-03 14:08:31 +0300

    Adjust calls order

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -11,100 +11,24 @@ namespace CoreOauth2Client
         private static void Main(string[] args)
         {
             var api = new OgoShipApi();
+            var merchantId = "{merchantId}";
+            var secredToken = "{secredToken}";
+            var applicationId = "123456";
+            var applicationSecret = "abcdef";
 
             var success = api.Authenticate(
-                "{merchantId}",
-                "{secredToken}",
-                "123456",
-                "abcdef",
-                "read:order read:product write:product write:order");
+                merchantId,
+                secredToken,
+                applicationId,
+                applicationSecret,
+                "read:order read:product write:product write:order read:webhook write:webhook");
 
             if (success)
             {
-                Console.WriteLine("Create new testproduct");
-                var product1 = new Product
-                {
-                    Code = DateTime.UtcNow.ToString(),
-                    Name = "Testproduct",
-                    LanguageCode = "en",
-                };
+                Console.WriteLine("Authentication done.");
 
-                var productResponse = (Product)api.AddProduct(product1);
-
-                Console.WriteLine("Rename and update product info");
-                var oldCode = productResponse.Code;
-                product1 = productResponse;
-                product1.Code = $"{productResponse.Code}new"; // Change SKU
-                product1.Name = "Test Product";
-                product1.Description = "Just test product";
-                product1.EanCode = "1234567890";
-                product1.Group = "Test products";
-                product1.InfoUrl = $"https://myshop.com/product/{product1.Code}";
-                product1.PictureUrl = $"https://myshop.com/product/{product1.Code}/img1.jpg";
-
-                product1.CountryOfOrigin = "cn";
-                product1.CurrencyCode = "EUR";
-                product1.SupplyPrice = 1.25M;
-                product1.Supplier = "Test supplier";
-                product1.SupplierCode = $"su{product1.Code}";
-
-                productResponse = api.UpdateProduct(productResponse, oldCode);
-
-                var order1 = new Order
-                {
-                    Reference = $"test{DateTime.UtcNow.ToString()}",
-                    Customer = new Customer
-                    {
-                        Address1 = "Teknobulevardi 3-5",
-                        City = "Vantaa",
-                        Zip = "01530",
-                        CountryCode = "fi",
-                        Name = "Test Person",
-                        Company = "OGOship",
-                        Email = "foo@bar.com",
-                        Phone = "+35810123456"
-                    },
-                    OrderLines = new List<OrderLine>
-                {
-                    new OrderLine
-                    {
-                        Code = product1.Code,
-                        Quantity = 10
-                    }
-                },
-                    PriceCurrency = "EUR",
-                    ShippingCode = "Test Deliverytype",
-                    Test = true
-                };
-
-                var response = api.AddOrder(order1);
-
-                Console.WriteLine("Get all orders");
-                var orders = api.GetOrders(modifiedAfter: new DateTime(2017, 1, 1));
-                foreach (var order in orders)
-                {
-                    Console.WriteLine($"{order.Reference}");
-                }
-
-                Console.WriteLine("Get all products by list");
-                var products = api.GetProducts();
-                foreach (var product in products)
-                {
-                    Console.WriteLine($"Check  {product.Code}");
-
-                    // Get product and stock by code
-                    var p = api.GetProducts(code: product.Code).FirstOrDefault();
-                    var s = api.GetStockLevels(productCode: product.Code).FirstOrDefault();
-
-                    if (p == null || p.Name != product.Name || s == null || s.StockAvailable != p.StockAvailable)
-                    {
-                        Console.WriteLine($"Problem with {product.Code}");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Product ok {product.Code}");
-                    }
-                }
+                WorkWithWebHooks(api);
+                GetProductsAndOrders(api);
             }
             else
             {
@@ -116,10 +40,98 @@ namespace CoreOauth2Client
             Console.ReadLine();
         }
 
-
-        private static void TestWebhooks(OgoShipApi api)
+        private static void GetProductsAndOrders(OgoShipApi api)
         {
-            var webHooksList = api.GetWebHooks();
+            Console.WriteLine("Create new testproduct");
+            var product1 = new Product
+            {
+                Code = DateTime.UtcNow.ToString(),
+                Name = "Testproduct",
+                LanguageCode = "en",
+            };
+
+            var productResponse = (Product)api.AddProduct(product1);
+
+            Console.WriteLine("Rename and update product info");
+            var oldCode = productResponse.Code;
+            product1 = productResponse;
+            product1.Code = $"{productResponse.Code}new"; // Change SKU
+            product1.Name = "Test Product";
+            product1.Description = "Just test product";
+            product1.EanCode = "1234567890";
+            product1.Group = "Test products";
+            product1.InfoUrl = $"https://myshop.com/product/{product1.Code}";
+            product1.PictureUrl = $"https://myshop.com/product/{product1.Code}/img1.jpg";
+
+            product1.CountryOfOrigin = "cn";
+            product1.CurrencyCode = "EUR";
+            product1.SupplyPrice = 1.25M;
+            product1.Supplier = "Test supplier";
+            product1.SupplierCode = $"su{product1.Code}";
+
+            productResponse = api.UpdateProduct(productResponse, oldCode);
+
+            var order1 = new Order
+            {
+                Reference = $"test{DateTime.UtcNow.ToString()}",
+                Customer = new Customer
+                {
+                    Address1 = "Teknobulevardi 3-5",
+                    City = "Vantaa",
+                    Zip = "01530",
+                    CountryCode = "fi",
+                    Name = "Test Person",
+                    Company = "OGOship",
+                    Email = "foo@bar.com",
+                    Phone = "+35810123456"
+                },
+                OrderLines = new List<OrderLine>
+                {
+                    new OrderLine
+                    {
+                        Code = product1.Code,
+                        Quantity = 10
+                    }
+                },
+                PriceCurrency = "EUR",
+                ShippingCode = "Test Deliverytype",
+                Test = true
+            };
+
+            var response = api.AddOrder(order1);
+
+            Console.WriteLine("Get all orders");
+            var orders = api.GetOrders(modifiedAfter: new DateTime(2017, 1, 1));
+            foreach (var order in orders)
+            {
+                Console.WriteLine($"{order.Reference}");
+            }
+
+            Console.WriteLine("Get all products by list");
+            var products = api.GetProducts();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Check  {product.Code}");
+
+                // Get product and stock by code
+                var p = api.GetProducts(code: product.Code).FirstOrDefault();
+                var s = api.GetStockLevels(productCode: product.Code).FirstOrDefault();
+
+                if (p == null || p.Name != product.Name || s == null || s.StockAvailable != p.StockAvailable)
+                {
+                    Console.WriteLine($"Problem with {product.Code}");
+                }
+                else
+                {
+                    Console.WriteLine($"Product ok {product.Code}");
+                }
+            }
+        }
+
+        private static void WorkWithWebHooks(OgoShipApi api)
+        {
+            List<WebHookResponse> webHooksList = api.GetWebHooks();
+
             var newHook = new WebHook()
             {
                 Url = "test",
@@ -127,9 +139,18 @@ namespace CoreOauth2Client
                 Type = WebhookType.OrderShipped
             };
 
-            var createdWebhook = api.CreateWebHook(newHook);
-            var webhook = api.GetWebHook(createdWebhook.Id);
+            WebHookResponse createdWebhook = api.CreateWebHook(newHook);
+
+            WebHookResponse webhook = api.GetWebHook(createdWebhook.Id);
+
+            webhook.Key = "TestTestTestKey";
+            WebHookResponse  updatedWebhook = api.UpdateWebHook(createdWebhook.Id, webhook);
+
+            webHooksList = api.GetWebHooks();
+
             api.DeleteWebHook(webhook.Id);
+
+            webHooksList = api.GetWebHooks();
         }
     }
 }
